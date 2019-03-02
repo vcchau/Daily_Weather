a31ad1fae9699b53ed1dec77e7244f8f3354e704
Victor Chau <vcchau@gmail.com>
2019-03-02 17:51:53 -0600

    Functional API calls onCreate, need to parse and show data

--- app/src/main/java/com/example/victor/dailyweather/MainActivity.java
+++ app/src/main/java/com/example/victor/dailyweather/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.victor.dailyweather;
 
+import android.net.Network;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        URL weatherURL = NetworkUtils.buildUrlForWeatherTwelveFourHour();
+        Log.i(TAG, "onCreate weatherURL: " + weatherURL);
     }
 }
---
+++ app/src/main/java/com/example/victor/dailyweather/NetworkUtils.java
@@ -0,0 +1,116 @@
+package com.example.victor.dailyweather;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    private final static String API_KEY = API_Key.getApi_key();
+
+    private final static String TAG = "NetworkUtils";
+
+    private final static String PARAM_API_KEY = "apikey";
+
+    /* ALL COMMENTED OUT STRINGS ARE CURRENTLY UNAUTHORIZED IN THE FREE API PACKAGE
+     * All string are missing location keys, we grab those by entering our city
+     */
+
+    // Base url for 1 Day of Daily Forecasts
+    private final static String ONE_DAY_BASE_REQUEST_URL =
+            "http://dataservice.accuweather.com/forecasts/v1/daily/1day/";
+
+    // Base url for 5 Days of Daily Forecasts
+    private final static String FIVE_DAY_BASE_REQUEST_URL =
+            "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
+
+    // Base url for 1 hour of Daily Forecasts
+    private final static String ONE_HOUR_BASE_REQUEST_URL =
+            "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/";
+
+    // Base url for 12 hours of Daily Forecasts
+    private final static String TWELVE_HOUR_BASE_REQUEST_URL =
+            "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
+
+    // Base url to search for location key
+    private final static String LOCATION_SEARCH_BASE_URL =
+            "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+    // Base url to search for current conditions
+    private final static String CURRENT_CONDITIONS_BASE_URL =
+            "http://dataservice.accuweather.com/currentconditions/v1/";
+
+//    // Base url for 24 hours of Daily Forecasts
+//    private final static String TWENTY_FOUR_HOUR_BASE_REQUEST_URL =
+//            "http://dataservice.accuweather.com/forecasts/v1/hourly/24hour/";
+
+//    // Base url for 72 hours of Daily Forecasts
+//    private final static String SEVENTY_TWO_HOUR_BASE_REQUEST_URL =
+//            "http://dataservice.accuweather.com/forecasts/v1/hourly/72hour/";
+
+//    // Base url for 120 hours of Daily Forecasts
+//    private final static String ONE_TWENTY_HOUR_BASE_REQUEST_URL =
+//            "http://dataservice.accuweather.com/forecasts/v1/hourly/120hour/";
+
+
+
+    // Static cache of popular cities
+    private final static HashMap<String, String> locationKeys = new HashMap<String, String>()
+    {{
+        put("austin", "351193");
+        put("houston", "351197");
+    }};
+
+    // Get info for twenty hours
+    public static URL buildUrlForWeatherTwelveFourHour() {
+        String requestURL = ONE_DAY_BASE_REQUEST_URL + locationKeys.get("austin");
+
+        // Use a uri to create our request url
+        Uri buildUri = Uri.parse(requestURL).buildUpon().appendQueryParameter(PARAM_API_KEY, API_KEY).build();
+
+        // Attempt to create the url
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // Log the url we built for debugging purposes
+        Log.i(TAG, "Twelve four hour request url created: " + url);
+
+        return url;
+    }
+
+    public static String getResponse(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            }
+            else {
+                return null;
+            }
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
+
